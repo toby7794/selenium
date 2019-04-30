0f24cef461a7dd23ce4dfecd12f6024b560fbd95
T L Goddard <TGoddard@spartaglobal.com>
2019-04-30 09:20:11 +0100

    initial commit

---
+++ src/main/java/com/spartaglobal/selenium/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
